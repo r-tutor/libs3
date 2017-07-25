ccf4c2dbebf5e4a8c5b1dcf5ed297b68292fd7e8
Auburngrads <auburngrads@live.com>
2017-07-25 03:41:15 -0400

    Update pkg js

---
+++ js/js/zzz.js
@@ -0,0 +1,20 @@
+/* Extra functions by Jeroen */
+UglifyJS.optimize = function(code, opts){
+  var ast = UglifyJS.parse(code);
+  var compressor = UglifyJS.Compressor(opts);
+  ast.figure_out_scope();
+  ast = ast.transform(compressor);
+  return ast.print_to_string();
+}
+
+UglifyJS.reformat = function(code, opts){
+  var ast = UglifyJS.parse(code);
+  var stream = UglifyJS.OutputStream(opts);
+  var code = ast.print(stream);
+  return stream.toString();
+}
+
+jshint.run = function(a,b,c) {
+  var err = jshint.JSHINT(a,b,c);
+  return err ? null : jshint.JSHINT.errors;
+}
