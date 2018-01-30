4c73730318727023c989e8e6cc73dccff5595af7
Auburngrads <auburngrads@live.com>
2018-01-30 15:54:28 -0500

    Update pkgload/tests/testthat/test-help.r

--- pkgload/tests/testthat/test-help.r
+++ pkgload/tests/testthat/test-help.r
@@ -48,9 +48,25 @@ test_that("show_help and shim_question files for devtools-loaded packages", {
   h1 <- shim_help("foofoo")
   expect_s3_class(h1, "dev_topic")
   expect_equal(h1$topic, "foofoo")
-  expect_equal(h1$pkg$package, "testHelp")
+  expect_equal(h1$pkg, "testHelp")
 
   expect_identical(shim_help(foofoo), h1)
   expect_identical(shim_help(foofoo, "testHelp"), h1)
   expect_identical(shim_question(testHelp::foofoo), h1)
+
+  pager_fun <- function(files, header, title, delete.file) {
+    expect_equal(title, "testHelp:foofoo.Rd")
+  }
+
+  withr::with_options(
+    c(pager = pager_fun),
+    print(h1, type = 'text'))
+})
+
+test_that("dev_help works with package and function help with the same name", {
+  load_all(test_path('testHelp'))
+  on.exit(unload(test_path('testHelp')))
+
+  h1 <- dev_help("testHelp")
+  expect_identical(shim_question(testHelp::testHelp), h1)
 })
